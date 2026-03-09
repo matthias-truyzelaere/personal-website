750f801b44ced0c63a60fc59fd18e7e46b132303
Matthias Truyzelaere <matthias.truyzelaere@gmail.com>
2026-03-09 20:54:03 +0100

    Add experimental inline CSS support in next.config.ts; update .gitignore to include agents directory

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	experimental: {
+		inlineCss: true,
+	},
+}
 
 export default nextConfig
 
